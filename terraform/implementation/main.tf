abc3046362434ee564e413ada1190ac42c4a4251
Kenneth Chow <kenneth@skylight.digital>
2023-08-22 09:32:23 -0500

    making validity changes

--- terraform/implementation/main.tf
+++ terraform/implementation/main.tf
@@ -1,15 +1,15 @@
 // Load modules here
 
 module "shared" {
-  source                     = "../modules/shared"
-  resource_group_name        = var.resource_group_name
-  location                   = var.location
-  smarty_auth_id             = var.smarty_auth_id
-  smarty_auth_token          = var.smarty_auth_token
-  smarty_license_type        = var.smarty_license_type
-  client_id                  = var.client_id
-  object_id                  = var.object_id
-  log_analytics_workspace_id = module.read_source_data.log_analytics_workspace_id
+  source              = "../modules/shared"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  smarty_auth_id      = var.smarty_auth_id
+  smarty_auth_token   = var.smarty_auth_token
+  smarty_license_type = var.smarty_license_type
+  client_id           = var.client_id
+  object_id           = var.object_id
+  # log_analytics_workspace_id = module.read_source_data.log_analytics_workspace_id
 }
 
 output "record_linkage_container_url" {
