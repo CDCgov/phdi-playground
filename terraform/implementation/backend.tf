941aa622b082b6442be7deeca88ea0de015017ea
Nick Clyde <nick@clyde.tech>
2023-08-25 09:47:00 -0700

    Add cert manager helm release

--- terraform/implementation/backend.tf
+++ terraform/implementation/backend.tf
@@ -12,6 +12,14 @@ terraform {
       source  = "azure/azapi"
       version = "= 1.8.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.10.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "= 2.23.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "= 3.5.1"
