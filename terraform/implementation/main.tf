daf0522ed1f47f83c6f848fa2ab1af8248467921
Nick Clyde <nick@clyde.tech>
2023-08-25 11:15:37 -0700

    Don't load config file

--- terraform/implementation/main.tf
+++ terraform/implementation/main.tf
@@ -218,6 +218,7 @@ provider "kubectl" {
   client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
   client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
+  load_config_file       = false
 }
 
 provider "helm" {
