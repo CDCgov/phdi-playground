8f08fc710f4edc787fff77abd7e0ffb3bcf35da8
Nick Clyde <nick@clyde.tech>
2023-09-05 14:34:31 -0700

    Remove pet name from SSH key

--- terraform/implementation/main.tf
+++ terraform/implementation/main.tf
@@ -76,11 +76,6 @@ resource "azurerm_role_assignment" "monitoring_reader" {
 }
 
 # SSH Key
-resource "random_pet" "ssh_key_name" {
-  prefix    = "ssh"
-  separator = ""
-}
-
 resource "azapi_resource_action" "ssh_public_key_gen" {
   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   resource_id = azapi_resource.ssh_public_key.id
@@ -92,7 +87,7 @@ resource "azapi_resource_action" "ssh_public_key_gen" {
 
 resource "azapi_resource" "ssh_public_key" {
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
+  name      = "phdi-playground-${terraform.workspace}-ssh-key"
   location  = var.location
   parent_id = data.azurerm_resource_group.rg.id
 }
