c45d21c3fbb82681d479f458ebc9aacfc643ed0e
Kenneth Chow <kenneth@skylight.digital>
2023-09-08 08:54:10 -0500

    adding skeleton

--- front-end/app/upload_file/page.tsx
+++ front-end/app/upload_file/page.tsx
@@ -1,11 +1,21 @@
 'use client'
-import { FileInput, FormGroup, Label } from '@trussworks/react-uswds'
+import { FileInput, FormGroup, Label, Button } from '@trussworks/react-uswds'
 
 export default function UploadFile(){
-    return <>
+    let fileToUpload: any = null
+    const addFile = (event: any): void => {
+        fileToUpload = event.target.files[0];
+    }
+
+    const uploadFile = (e: any): void => {
+        console.log(fileToUpload)
+    }
+
+    return <div>
         <FormGroup>
             <Label htmlFor="file-input-single">Input accepts a single file</Label>
-            <FileInput id="file-input-single" name="file-input-single" />
+            <FileInput id="file-input-single" name="file-input-single" onChange={(addFile)}/>
+            <Button type="button" disabled={fileToUpload} onClick={uploadFile}>Upload</Button>
         </FormGroup>
-    </>
+    </div>
 }
