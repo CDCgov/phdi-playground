4a3312ef7548aa8151fe821916bd1b9dc15d03a2
Kenneth Chow <kenneth@skylight.digital>
2023-10-05 10:08:46 -0500

    upload functionality

--- front-end/app/upload_file/page.tsx
+++ front-end/app/upload_file/page.tsx
@@ -1,21 +1,47 @@
 'use client'
 import { FileInput, FormGroup, Label, Button } from '@trussworks/react-uswds'
+import { useState } from 'react';
+
 
 export default function UploadFile() {
-    let fileToUpload: any = null
+    const process_url = 'http://localhost:8080/process'
+    const [file, setFile] = useState(null);
     const addFile = (event: any): void => {
-        fileToUpload = event.target.files[0];
+        setFile(event.target.files[0]);
     }
 
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        if (file) {
+            const formData = new FormData();
+            formData.append('upload_file', file);
+            formData.append('message_type', 'ecr');
+            formData.append('include_error_types', 'errors');
+            console.log('i am here')
+            try {
+                const response = await fetch(process_url, {
+                    method: 'POST',
+                    body: formData,
+                });
+
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error('Error uploading file', error);
+            }
+        }
+    };
+
     const uploadFile = (e: any): void => {
-        console.log(fileToUpload)
+        console.log(file)
     }
 
     return <div className="margin-3">
         <FormGroup>
             <Label htmlFor="file-input-single">Input accepts a single file</Label>
             <FileInput id="file-input-single" name="file-input-single" onChange={(addFile)} />
-            <Button type="button" disabled={fileToUpload} onClick={uploadFile}>Upload</Button>
+            <Button type="button" onClick={handleSubmit}>Upload</Button>
         </FormGroup>
     </div>
 }
