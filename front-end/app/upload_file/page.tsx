55fa8f50d35cbff933faf635a0b59a59326bac55
nickbristow <richard.nick.bristow@gmail.com>
2023-10-25 16:53:31 -0400

    wip

--- front-end/app/upload_file/page.tsx
+++ front-end/app/upload_file/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { useData } from '@/utils/DataContext';
-import { FileInput, FormGroup, Alert, Button } from '@trussworks/react-uswds'
-import { useState } from 'react';
+
+import { FileInput, FormGroup, Label, Button, Alert } from '@trussworks/react-uswds'
+import { useState, useRef, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import SingleFileInput from '@/components/SingleFileInput/SingleFileInput';
 import LinkAccordion from '@/components/LinkAccordion/LinkAccordion';
@@ -9,58 +10,84 @@ import LinkAccordion from '@/components/LinkAccordion/LinkAccordion';
 
 
 export default function UploadFile() {
-    const { setData } = useData();
-    const router = useRouter();
-    // We will change this and put it in a constants 
-    // file when orchestration is published
-    const process_url = 'http://localhost:8080/process'
-    const [file, setFile] = useState<File | null>(null);
-    const addFile = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        setFile(event.target.files?.item(0) || null);
-    }
+  const process_url = 'ws://localhost:8080/process-ws'
+  const [formData, setFormData] = useState({}); // State for form data
+  const [progress, setProgress] = useState(0); // State for progress
+  const [socket, setSocket] = useState(null);
+  const fileInputRef = useRef(); // Define the ref
+  const [file, setFile] = useState(null);
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        if (file) {
-            const formData = new FormData();
-            formData.append('upload_file', file);
-            formData.append('message_type', 'ecr');
-            formData.append('include_error_types', 'errors');
-            try {
-                const response = await fetch(process_url, {
-                    method: 'POST',
-                    body: formData,
-                });
-                const data = await response.json();
-                setData(data);
-                router.push('/export')
-            } catch (error) {
-                console.error('Error uploading file', error);
-            }
-        }
+  const handleSubmit = async (e: any) => {
+      e.preventDefault();
+      if (file) {
+          const formData = new FormData();
+          formData.append('upload_file', file);
+          formData.append('message_type', 'ecr');
+          formData.append('include_error_types', 'errors');
+          try {
+              const response = await fetch(process_url, {
+                  method: 'POST',
+                  body: formData,
+              });
+              const data = await response.json();
+              setData(data);
+              router.push('/export')
+          } catch (error) {
+              console.error('Error uploading file', error);
+          }
+      }
+  } 
+  const handleFileChange = (e: any) => {
+      const selectedFile = e.target.files[0];
+      setFile(selectedFile);
+  };
+
+  useEffect(() => {
+    const ws = new WebSocket(url);
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setProgress(data["progress"]);
     };
 
-    return (
-        <div>
-          <h1 className='margin-bottom-1'>Upload your eCR</h1>
-          <p>Select an eCR .zip file to process</p>
-          <div className="usa-alert usa-alert--info margin-bottom-1">
-            <div className="usa-alert__body">
-              <p className="usa-alert__text">
-                This tool is only for test data. Please do not upload patient data to this site.
-              </p>
+    ws.onclose = () => {
+      // Handle WebSocket closed
+    };
+
+    setSocket(ws);
+
+    return () => {
+      ws.close(); // Close the WebSocket when the component unmounts
+    };
+  }, []);
+    if(progress){
+      return (
+        {progress}
+      )
+    } else {
+      return (
+          <div>
+            <h1 className='margin-bottom-1'>Upload your eCR</h1>
+            <p>Select an eCR .zip file to process</p>
+            <div className="usa-alert usa-alert--info margin-bottom-1">
+              <div className="usa-alert__body">
+                <p className="usa-alert__text">
+                  This tool is only for test data. Please do not upload patient data to this site.
+                </p>
+              </div>
             </div>
+              <FormGroup>
+                  <Label htmlFor="file-input-single">Input accepts a single file</Label>
+                  <FileInput 
+                      id="file-input-single"
+                      name="file-input-single"
+                      onChange={(handleFileChange)}
+                      className='width-lg margin-bottom-1'
+                      itemRef='fileInputRef'
+                  />
+                  <Button type="button" onClick={handleSubmit}>Upload</Button>
+              </FormGroup>
           </div>
-            <FormGroup>
-                <FileInput id="file-input-single"
-                    name="file-input-single" onChange={(addFile)}
-                    className='width-lg margin-bottom-1'
-                />
-                <div className="margin-top-205">
-                    <LinkAccordion></LinkAccordion>
-                </div>
-                <Button className="margin-top-3" disabled={!file} type="button" onClick={handleSubmit}>Continue</Button>
-            </FormGroup>
-        </div>
-    )
+      )
+    }
 }
---
+++ front-end/app/ws/page.tsx
@@ -0,0 +1,83 @@
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
+import { FileInput, FormGroup, Label, Button } from '@trussworks/react-uswds'
+
+function Ws() {
+  const url = 'ws://localhost:8080/process-ws'
+  const [formData, setFormData] = useState({}); // State for form data
+  const [progress, setProgress] = useState(0); // State for progress
+  const [socket, setSocket] = useState(null);
+   const fileInputRef = useRef(); // Define the ref
+
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+    };
+
+  const handleSubmit = () => {
+    // Send form data to the server via a WebSocket
+    const formData = new FormData();
+  // Add the file data to the FormData object (assuming you have a file input with the name "file")
+
+      console.log("What is file?", file)
+
+    const fileReader = new FileReader()
+    formData.append("file", file);
+    
+
+      formData.forEach((value, key) => {
+          console.log(key, value)})
+
+    socket.send(file)
+  };
+
+  useEffect(() => {
+    const ws = new WebSocket(url);
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setProgress(data["progress"]);
+    };
+
+    ws.onclose = () => {
+      // Handle WebSocket closed
+    };
+
+    setSocket(ws);
+
+    return () => {
+      ws.close(); // Close the WebSocket when the component unmounts
+    };
+  }, []);
+  let fileToUpload: any = null
+
+
+  if(progress === 0){
+      return (
+        <div className="margin-3">
+            <FormGroup>
+                <Label htmlFor="file-input-single">Input accepts a single file</Label>
+                <FileInput
+                  id="file-input-single"
+                  name="file-input-single"
+                  onChange={(handleFileChange)}
+                  itemRef='fileInputRef'
+                />
+                <Button type="button" disabled={fileToUpload} onClick={handleSubmit}>Upload</Button>
+            </FormGroup>
+        </div>
+      );
+  } else {
+    return (
+      <>
+        <p>{progress}</p>
+      </>
+    )
+  }
+
+  
+}
+
+export default Ws;
