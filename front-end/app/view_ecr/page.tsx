5317714c3633af011a7a6a44fd9e4d81191005ad
Josh Nygaard <jnygaard@skylight.digital>
2023-10-30 14:44:38 -0400

    dummy ecr

--- front-end/app/view_ecr/page.tsx
+++ front-end/app/view_ecr/page.tsx
@@ -1,17 +1,31 @@
 'use client'
 import {Button} from '@trussworks/react-uswds'
+import ECRTable from '@/components/ECRTable';
+import React from "react";
 
 export default function ViewECR() {
+	// const {data} = useData()
+	const data = {
+		"processed_values": {
+			"message": "Parsing succeeded!",
+			"parsed_values": {
+				"patient_id": "123",
+				"person_id": null,
+				"last_name": "BEAKER",
+				"first_name": "CATEST",
+				"birth_date": "2016-01-27",
+				"gender": "female"
+			}
+		}
+	};
+
 	return (
 		<div className="margin-3">
-			<h1>View your eCR</h1>
+			<h1 className={'font-sans-xl text-bold'}>View your eCR</h1>
 			<p>
 				You can view your data below or download it as a FHIR bundle (JSON file).
 			</p>
-
-			{/*<ECRTable>*/}
-			{/*</ECRTable>*/}
-
+			<ECRTable ecrData={data}></ECRTable>
 			<Button type="button">Upload new eCR</Button>
 			<Button type="button">Download FHIR bundle</Button>
 		</div>
