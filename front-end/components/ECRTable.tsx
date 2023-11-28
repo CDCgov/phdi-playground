e9a69f891d46dbb3794868657a36912e9a34dc19
Josh Nygaard <jnygaard@skylight.digital>
2023-11-28 14:31:00 -0500

    ecr veiwer

--- front-end/components/ECRTable.tsx
+++ front-end/components/ECRTable.tsx
@@ -1,45 +1,41 @@
-import {
-    Table
-} from '@trussworks/react-uswds'
+import {Table} from '@trussworks/react-uswds'
 import _ from 'lodash';
 
-export default function ECRTable({ ecrData }) {
-    const options = ['patient_id', 'first_name', 'last_name', 'gender', 'birth_date']
+export default function ECRTable({ecrData}) {
+	const options = ['patient_id', 'first_name', 'last_name', 'gender', 'birth_date']
 
 
-    const getTableBody = (data: any) => {
-        return (
-            <tbody>
-                {options.map(function (option) {
-                    return (
-                        <tr key={option}>
-                            <th scope="row">{_.startCase(option)}</th>
-                            <td>{data[option]}</td>
-                        </tr>
-                    );
-                })}
+	const getTableBody = (data: any) => {
+		return (
+			<tbody>
+			{options.map(function (option) {
+				return (
+					<tr key={option}>
+						<th scope="row">{_.startCase(option)}</th>
+						<td>{data[option]}</td>
+					</tr>
+				);
+			})}
 
-            </tbody>
-        )
-    }
+			</tbody>
+		)
+	}
 
-    return (
-        <div className='margin-3'>
-            <h1>eCR Viewer</h1>
-            <div>
-                <Table
-                    bordered
-                    caption="This table uses the fullWidth prop to increase to 100% width"
-                    fullWidth>
-                    <thead>
-                        <tr>
-                            <th scope="col">Field Name</th>
-                            <th scope="col">Field Value</th>
-                        </tr>
-                    </thead>
-                    {getTableBody(ecrData.processed_values.parsed_values)}
-                </Table>
-            </div>
-        </div>
-    )
+	return (
+		<div className='margin-3'>
+			<div>
+				<Table
+					bordered
+					fullWidth>
+					<thead>
+					<tr>
+						<th scope="col">Field Name</th>
+						<th scope="col">Field Value</th>
+					</tr>
+					</thead>
+					{getTableBody(ecrData.processed_values.parsed_values)}
+				</Table>
+			</div>
+		</div>
+	)
 }
