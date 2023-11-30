78f31989cdee402c188c868cc20b6fd5febed947
Kenneth Chow <kenneth@skylight.digital>
2023-11-30 12:43:02 -0600

    moving back to main

--- terraform/implementation/main.tf
+++ terraform/implementation/main.tf
@@ -473,7 +473,7 @@ resource "azurerm_linux_web_app" "playground_webapp" {
 resource "azurerm_app_service_source_control" "playground_sourcecontrol" {
   app_id                 = azurerm_linux_web_app.playground_webapp.id
   repo_url               = "https://github.com/CDCgov/phdi-playground"
-  branch                 = "deployment-web-app"
+  branch                 = "main"
   use_manual_integration = false
   use_mercurial          = false
 }
