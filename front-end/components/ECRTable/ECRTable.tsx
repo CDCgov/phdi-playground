8d6799a14a335bdee590585a7820955ebc9b03fc
Zedd Shmais <zdveloper@gmail.com>
2024-01-08 13:34:32 -0600

    Enhance ecr viewer to show all fields (#40)
    * enhance ecr viewer
    
    * enhance the ecr viewer page to show the entire ecr
    
    * add tests and fix footer
    
    * update css and tests
    
    * updated the styles
    
    * cleanup
    
    * correctly format eICR RR
    
    * code cleanup
    
    * formatting

--- front-end/components/ECRTable/ECRTable.tsx
+++ front-end/components/ECRTable/ECRTable.tsx
@@ -1,37 +1,106 @@
-import {Table} from '@trussworks/react-uswds'
+import {
+	Table
+} from '@trussworks/react-uswds'
 import _ from 'lodash';
-
-export default function ECRTable({ecrData}: any) {
-
-	const options = ['patient_id', 'first_name', 'last_name', 'gender', 'birth_date']
+import { v4 } from 'uuid';
 
 
-	const getTableBody = (data: any) => {
+export default function ECRTable({ ecrData }: any) {
+	const tableData = ecrData?.processed_values?.parsed_values
+	// remove null fields
+	Object.keys(tableData || {}).forEach((k) => tableData[k] == null && delete tableData[k])
+	const options = Object.keys(tableData || {});
+	const arrayOptions = options.filter((option) => Array.isArray(tableData[option]))
+	// remove array fields from options
+	arrayOptions.forEach(arrayOption => {options.splice(options.indexOf(arrayOption), 1)})
+	const formatHeader = (innerOption: string) => _.startCase(innerOption)
+		.replace(/rr /i, "RR ")
+		.replace(/eicr /i, "eICR ")
+		.replace(/ecr /i, "eCR ");
+
+	const getArrayTableHeader = (data: [{[key: string]: any}]) => {
+		const innerOptions = data.length > 0 && Object.keys(data[0] || {}) || [];
+
+		return (
+			<thead>
+				<tr>
+					{innerOptions.map(innerOption => <th scope="col" key={v4()}>{formatHeader(innerOption)}</th>)}
+				</tr>
+			</thead>
+		)
+	}
+
+	const getArrayTableBody = (data: [{[key: string]: any}]) => {
+		const innerOptions = data.length > 0 && Object.keys(data[0] || {}) || [];
+
 		return (
 			<tbody>
-			{data && options.map(function (option) {
+			{data.map(entry => (
+				<tr key={v4()}>
+					{innerOptions.map(option => {
+						return (<th scope="row" key={v4()}>{entry[option]}</th>)
+					})}
+				</tr>
+			))}
+
+			</tbody>
+		)
+	}
+
+	const getTableBody = (data: any) => {
+		Object.keys(data || {}).forEach((k) => data[k] == null && delete data[k])
+		const innerOptions = Object.keys(data || {});
+		const innerArrayOptions = innerOptions.filter((option) => Array.isArray(data[option]))
+		innerArrayOptions.forEach(arrayOption => {innerOptions.splice(innerOptions.indexOf(arrayOption), 1)})
+		return (
+			<tbody>
+			{data && innerOptions.map(function (option) {
 				return (
 					<tr key={option}>
-						<th scope="row">{_.startCase(option)}</th>
+						<th scope="row">{formatHeader(option)}</th>
 						<td>{data[option]}</td>
 					</tr>
 				);
 			})}
+
 			</tbody>
 		)
 	}
+
 	return (
-		<Table className={'usa-table'}
-					 bordered
-					 fullWidth>
-			<thead>
-			<tr>
-				<th scope="col">Field Name</th>
-				<th scope="col">Field Value</th>
-			</tr>
-			</thead>
-			
-			{getTableBody(ecrData?.processed_values?.parsed_values)}
-		</Table>
+		<div className='margin-top-3'>
+			<h1>eCR Viewer</h1>
+			<div>
+				<Table
+					bordered
+					caption="This table uses the fullWidth prop to increase to 100% width"
+					fullWidth>
+					<thead>
+					<tr>
+						<th scope="col">Field Name</th>
+						<th scope="col">Field Value</th>
+					</tr>
+					</thead>
+					{getTableBody(tableData)}
+				</Table>
+			</div>
+			{arrayOptions.map(option => {
+				return (
+					<div key={option}>
+						<h2>{formatHeader(option)}</h2>
+						<div>
+							<Table
+								bordered
+								scrollable
+								fullWidth
+								className="no-wrap-table"
+							>
+								{getArrayTableHeader(tableData[option])}
+								{getArrayTableBody(tableData[option])}
+							</Table>
+						</div>
+					</div>)
+			})}
+		</div>
 	)
 }
