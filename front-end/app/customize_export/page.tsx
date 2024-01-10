59dafde071d07ba5f8a63904ead96c193dcf6b03
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-01-10 18:43:35 -0500

    adding customized exits

--- front-end/app/customize_export/page.tsx
+++ front-end/app/customize_export/page.tsx
@@ -5,98 +5,134 @@ import _ from 'lodash';
 import { Checkbox, Button } from '@trussworks/react-uswds'; // https://designsystem.digital.gov/components/checkbox/
 import './page.scss' 
 import { json } from 'stream/consumers';
+import ecrData from './data.json'; // test data; remove once complete
 
 // read in message-parser bundle and parse
 // const ecrData = useData(); // Access the shared data;
-const ecrData = {"message":"Processing succeeded!","processed_values":{"message":"Parsing succeeded!","parsed_values":{"patient_id":"9483a462-949c-4759-a18b-35da40c31f2f","last_name":"ZZTESTTWELVE","first_name":"CEDARSECR","birth_date":"1990-12-31","gender":"female","race":"Black or African American","ethnicity":"Hispanic or Latino","street_address1":"1123 MALTMAN STREET","state":"CA","zip":"90026","rr_id":"81109d01-969d-42a2-9cc2-5c20d05eb184","status":"RRVS19","conditions":"Disease caused by severe acute respiratory syndrome coronavirus 2 (disorder)","eicr_set_id":"e1634a9e-128f-11ed-b2a6-005056a7fb21","eicr_id":"1.2.840.114350.1.13.202.3.7.8.688883.314","eicr_version_number":"4","replaced_eicr_id":"Composition/1.2.840.114350.1.13.202.3.7.8.688883.313","replaced_eicr_version":"3","authoring_datetime":"2022-08-02T15:25:00-07:00","provider_id":"11003564","facility_id_number":"1.2.840.114350.1.13.202.3.7.2.686980","facility_name":"CSMG Internal Medicine","facility_type":"Healthcare Facility","encounter_type":"Ambulatory","encounter_start_date":"2022-08-02T11:00:00-07:00","encounter_end_date":"2022-08-02T11:20:00-07:00","active_problems":[{"problem":"Mild to moderate pre-eclampsia","problem_date":"2022-07-21"},{"problem":"Depression, recurrent","problem_date":"2022-07-21"},{"problem":"Pain, upper back","problem_date":"2022-07-22"},{"problem":"2019 novel coronavirus disease (COVID-19)","problem_date":"2022-08-02"},{"problem":"Generalized abdominal pain","problem_date":"2022-08-02"},{"problem":"Eclampsia, antepartum","problem_date":"2022-08-02"},{"problem":"Pregnancy related exhaustion and fatigue, antepartum","problem_date":"2022-08-02"},{"problem":"Pregnancy test positive","problem_date":"2022-08-02"}],"labs":[],"preferred_language":"English"}},"complete":true};
 const tableData = ecrData?.processed_values?.parsed_values
 
 export default function CheckboxesPage() {
-  const renderCheckboxes = () => {
-    // remove null fields and standardize naming
-    Object.keys(tableData || {}).forEach((k) => tableData[k] == null && delete tableData[k]);
-    const options = Object.keys(tableData || {});
-    const formatHeader = (innerOption: string) => _.startCase(innerOption)
-      .replace(/rr /i, "RR ")
-      .replace(/eicr /i, "eICR ")
-      .replace(/ecr /i, "eCR ");
+  const [keysDict, setKeysDict] = useState({});
+  const [checkedItems, setCheckedItems] = useState({});
 
-    // TODO: remove array fields from options, present as separate sections like ECRTable.tsx
-    const arrayOptions = options.filter((option) => Array.isArray(tableData[option]));
-    arrayOptions.forEach(arrayOption => { options.splice(options.indexOf(arrayOption), 1) });
-
-    // Initialize checkedItems with all options checked by default
-    const initialCheckedItems = options.reduce((acc, option) => {
-      acc[option] = true;
-      return acc;
-    }, {});
-    const [checkedItems, setCheckedItems] = useState(initialCheckedItems);
-
-    // add deselect button
-    const handleCheckboxChange = (event) => {
-      const { name, checked } = event.target;
-      setCheckedItems((prevCheckedItems) => ({ ...prevCheckedItems, [name]: checked }));
-    };
-    const deselectAll = () => {
-      const uncheckedItems = options.reduce((acc, option) => {
-        acc[option] = false;
-        return acc;
-      }, {});
-      setCheckedItems(uncheckedItems);
-    };
-
-    // add checkboxes
-    const checkboxes = options.map((option) => (
-      <Checkbox
-        key={option}
-        id={`checkbox-${option}`}
-        name={option}
-        label={formatHeader(option)}
-        checked={checkedItems[option] || false}
-        onChange={handleCheckboxChange}
-      />
-    ));
-    
-    // get each item in the array as checkboxes
-    const initialCheckedItemsArray = arrayOptions.reduce((acc, option) => {
-      acc[option] = true;
-      return acc;
-    }, {});
-    const [checkedItemsArray, setCheckedItemsArray] = useState(initialCheckedItemsArray);    
-    const handleCheckboxChangeArray = (event) => {
-      const { name, checked } = event.target;
-      setCheckedItemsArray((prevCheckedItems) => ({ ...prevCheckedItems, [name]: checked }));
-    };
-    const getArrayCheckboxes = arrayOptions.map((option) => (
-        <Checkbox
-          key={option}
-          id={`checkbox-${option}`}
-          name={option}
-          label={formatHeader(option)}
-          checked={checkedItemsArray[option] || false}
-          onChange={handleCheckboxChangeArray}
-        />
-      ));
-    console.log(getArrayCheckboxes)
-
-    const deselectAllArray = () => {
-      const uncheckedItemsArray = arrayOptions.reduce((acc, option) => {
-        acc[option] = false;
-        return acc;
-      }, {});
-      setCheckedItemsArray(uncheckedItemsArray);
-    };    
-    return { checkboxes, checkedItems, getArrayCheckboxes };
+  // Handle checkbox change
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setCheckedItems((prev) => ({ ...prev, [name]: checked }));
   };
+
+  useEffect(() => {
+    const extractKeys = () => {
+      const keysDictTemp = {};
+
+      // Directly access and process 'parsed_values'
+      if (tableData) {
+        keysDictTemp['parsed_values'] = Object.keys(tableData || {}).filter((key) => !Array.isArray(tableData[key]));
+      }
+      // Directly access and process 'labs'
+      if (tableData.labs && Array.isArray(tableData.labs) && tableData.labs.length > 0) {
+        const labKeys = new Set();
+        tableData.labs.forEach((item) => {
+          Object.keys(item).forEach((key) => labKeys.add(key));
+        });
+        keysDictTemp['labs'] = Array.from(labKeys);
+      }
+
+      // Directly access and process 'active_problems'
+      if (
+        tableData.active_problems &&
+        Array.isArray(tableData.active_problems) &&
+        tableData.active_problems.length > 0
+      ) {
+        const problemKeys = new Set();
+        tableData.active_problems.forEach((item) => {
+          Object.keys(item).forEach((key) => problemKeys.add(key));
+        });
+        keysDictTemp['active_problems'] = Array.from(problemKeys);
+      }
+
+      return keysDictTemp;
+    };
+
+    const keys = extractKeys();
+    setKeysDict(keys);
+  }, []);
+
+  // Initialize all checkboxes as checked
+  useEffect(() => {
+    const initialChecked = Object.values(keysDict).flat().reduce((acc, key) => {
+      acc[key] = true;
+      return acc;
+    }, {});
+    setCheckedItems(initialChecked);
+  }, [keysDict]);
+
+  const deselectAll = (section) => {
+    const uncheckedItems = keysDict[section].reduce((acc, option) => {
+      acc[option] = false;
+      return acc;
+    }, {});
+    setCheckedItems((prev) => ({ ...prev, ...uncheckedItems }));
+  };
+
+
+  // Create checkbox components
+  const createCheckboxes = () => {
+    return Object.keys(keysDict).map((section) => (
+      <div key={section}>
+        <div className="deselect-all-container">
+          <th>{_.startCase(section)}</th>
+          <button className="deselect-all-button" onClick={() => deselectAll(section)}>
+            Deselect All
+          </button>
+        </div>
+        <div className="checkbox-container">
+          {keysDict[section].map((key) => (
+            <div key={key}>
+              <Checkbox
+                id={`checkbox-${key}`}
+                name={key}
+                label={_.startCase(key).replace(/rr /i, "RR ")
+                .replace(/eicr /i, "eICR ")
+                .replace(/ecr /i, "eCR ")}
+                checked={checkedItems[key]}
+                onChange={handleCheckboxChange}
+              />
+            </div>
+          ))}
+        </div>
+        <br />
+      </div>
+    ));
+  };
+
+  const checkboxes = createCheckboxes();
 
   // code taken from export page to establish button
   const downloadFile = () => {
-    // Convert the data object to a JSON string
-    const jsonData = JSON.stringify(tableData);
-
-    // TODO Filter out unchecked items
-    // const { checkedItems } = renderCheckboxes();
+    // Filter out unchecked items -- TODO, make lab and active problems dynamic to account for future arrays
     const filteredData = tableData;
+    for (const key in checkedItems) {
+      if (!checkedItems[key]) {
+        delete filteredData[key];
+    
+        if (filteredData.labs && Array.isArray(filteredData.labs)) {
+          filteredData.labs.forEach((lab) => {
+            if (lab.hasOwnProperty(key)) {
+              delete lab[key];
+            }
+          });
+        }
+    
+        if (filteredData.active_problems && Array.isArray(filteredData.active_problems)) {
+          filteredData.active_problems.forEach((problem) => {
+            if (problem.hasOwnProperty(key)) {
+              delete problem[key];
+            }
+          });
+        }
+      }
+    }
     const selectedData = JSON.stringify(filteredData);
 
     // Create a Blob with the JSON data
@@ -117,50 +153,13 @@ export default function CheckboxesPage() {
     URL.revokeObjectURL(url);
   };
 
-  const { checkboxes, deselectAll, getArrayCheckboxes, deselectAllArray } = renderCheckboxes();
-
   return (
     <div>
       <h1>Customize your bundle</h1>
-      <h3>Select which fields wish to <i><b>omit</b></i> from the options below:</h3>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' , background: '#f0f0f080'}}>
-        <div style={{ marginLeft: 'auto' }}>
-          <a href="#" onClick={deselectAll} style={{ color: '#0000FF', textDecoration: 'underline' }}>Deselect All</a>
-        </div>
-      </div>
-      <div className="checkbox-container">
+      <h3>Select which fields you wish to <i><b>omit</b></i> from the options below:</h3>
         {checkboxes}
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' , background: '#f0f0f080'}}>
-        <div style={{ marginLeft: 'auto' }}>
-          <a href="#" onClick={deselectAllArray} style={{ color: '#0000FF', textDecoration: 'underline' }}>Deselect All</a>
-        </div>
-      </div>      
-      <div className="checkbox-container">
-        {getArrayCheckboxes}
-      </div>      
-      <br />
       <Button type="button" onClick={downloadFile}>Download (.json)</Button>
+      <Button type="button" className="usa-button--outline usa-button" onClick={() => location.reload()}>Cancel</Button>
     </div>
-
   );
-};
-
-
-// scratch
-      // TODO Filter out unchecked items
-      // const checkedItems = renderCheckboxes().checkedItems;
-      // const filteredData = Object.keys(checkedItems).reduce((acc, key) => {
-      //   const isChecked = checkedItems[key];    
-      //   if (isChecked) {
-      //     // If the field is an array, filter the array based on checkedItems
-      //     if (Array.isArray(tableData[key])) {
-      //       acc[key] = tableData[key].filter((item, index) => checkedItems[`${key}_${index}`])
-      //     } else {
-      //       // Otherwise, include the entire field
-      //       acc[key] = tableData[key];
-      //     }
-      //   }
-    
-      //   return acc;
-      // }, {});
+}
