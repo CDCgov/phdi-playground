af5dc8fdde305189da431a77a8b035029d9a42c9
Nick Clyde <nick@clyde.tech>
2024-01-10 14:57:09 -0800

    Create an EKS cluster in AWS

--- terraform/aws/implementation/main.tf
+++ terraform/aws/implementation/main.tf
@@ -2,3 +2,9 @@ module "vpc" {
   source = "./modules/vpc"
   region = var.region
 }
+
+module "eks" {
+  source            = "./modules/eks"
+  region            = var.region
+  public_subnet_ids = module.vpc.public_subnet_ids
+}
