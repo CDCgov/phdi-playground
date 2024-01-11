ccfb1b3876318cc60030db3883b3ed6937c59c58
Nick Clyde <nick@clyde.tech>
2024-01-11 10:51:31 -0800

    Create an EKS cluster in AWS (#45)
    * Create an EKS cluster in AWS
    
    * terraform fmt

---
+++ terraform/aws/implementation/modules/eks/main.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_role" "eks" {
+  name               = "eks-cluster-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "amazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "amazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "phdi-playground-eks-${terraform.workspace}"
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    subnet_ids = var.public_subnet_ids
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.amazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.amazonEKSVPCResourceController,
+  ]
+}
---
+++ terraform/aws/implementation/modules/eks/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
