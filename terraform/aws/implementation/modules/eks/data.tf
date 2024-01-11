a563ab23828ba1bf452b011d5b45116187ca7ae1
Nick Clyde <nick@clyde.tech>
2024-01-11 10:51:31 -0800

    Create an EKS cluster in AWS (#45)
    * Create an EKS cluster in AWS
    
    * terraform fmt

---
+++ terraform/aws/implementation/modules/eks/data.tf
@@ -0,0 +1,12 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
