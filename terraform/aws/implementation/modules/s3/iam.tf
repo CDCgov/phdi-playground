f9d31f71ebe94d4fd98a7aa2a6a32d71e6172f0f
marycrawford <mary.crawford@skylight.digital>
2024-02-26 00:10:56 -0800

    update bucket id attribute

--- terraform/aws/implementation/modules/s3/iam.tf
+++ terraform/aws/implementation/modules/s3/iam.tf
@@ -1,6 +1,6 @@
 # TODO iam permissions to speak with clear
 resource "aws_s3_bucket_policy" "allow_s3_access_for_ecr_viewer" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.s3.id
   policy = data.aws_iam_policy_document.ecr_viewer_s3_policy.json
 }
 
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "ecr_viewer_s3_policy" {
 }
 
 resource "aws_s3_bucket_policy" "allow_s3_access_for_orchestration" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.s3.id
   policy = data.aws_iam_policy_document.orchestration_s3_policy.json
 }
 
@@ -50,7 +50,7 @@ data "aws_iam_policy_document" "orchestration_s3_policy" {
 }
 
 resource "aws_s3_bucket_policy" "allow_s3_access_for_fhir_converter" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.s3.id
   policy = data.aws_iam_policy_document.fhir_converter_s3_policy.json
 }
 
