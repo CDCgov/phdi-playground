d95c24ae027aea9124cf4152d19e694c879c79a3
marycrawford <mary.crawford@skylight.digital>
2024-02-26 00:27:16 -0800

    update s3 iam policies for ecrViewer

--- terraform/aws/implementation/modules/s3/iam.tf
+++ terraform/aws/implementation/modules/s3/iam.tf
@@ -8,10 +8,14 @@ data "aws_iam_policy_document" "ecr_viewer_s3_policy" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = ["ecrViewerGetS3Policy"]
+      identifiers = ["ecrViewerPutPostGetS3Policy"]
     }
 
     actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:PostObject",
+      "s3:PostObjectAcl",
       "s3:GetObject",
       "s3:GetObjectAcl",
     ]
@@ -39,30 +43,6 @@ data "aws_iam_policy_document" "orchestration_s3_policy" {
       "s3:PutObject",
       "s3:PutObjectAcl",
       "s3:PostObject",
-      "s3:PostObjectAcl",
-    ]
-
-    resources = [
-      aws_s3_bucket.s3.arn,
-      "${aws_s3_bucket.s3.arn}/*",
-    ]
-  }
-}
-
-resource "aws_s3_bucket_policy" "allow_s3_access_for_fhir_converter" {
-  bucket = aws_s3_bucket.s3.id
-  policy = data.aws_iam_policy_document.fhir_converter_s3_policy.json
-}
-
-data "aws_iam_policy_document" "fhir_converter_s3_policy" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["fhirConverterPostS3Policy"]
-    }
-
-    actions = [
-      "s3:PotObject",
       "s3:PostObjectAcl",
     ]
 
