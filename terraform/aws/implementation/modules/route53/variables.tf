063474dad0ed16f962899e3ba2479b12ca771dba
Nick Clyde <nick@clyde.tech>
2024-02-27 16:15:17 -0800

    Use ACM certificate with ALB instead of Cloudfront

---
+++ terraform/aws/implementation/modules/route53/variables.tf
@@ -0,0 +1,10 @@
+variable "domain_name" {
+  description = "The domain name to use for the Route53 hosted zone"
+  type        = string
+  default = "dibbs.cloud"
+}
+
+variable "alb_hostname" {
+  description = "The hostname of the ALB"
+  type        = string
+}
