8cd85ea9a4948db1ed2fad11efb35b33456eaba7
marycrawford <mary.crawford@skylight.digital>
2024-02-29 13:02:53 -0800

    update s3 iam roles and variables files

--- terraform/aws/implementation/modules/s3/iam.tf
+++ terraform/aws/implementation/modules/s3/iam.tf
@@ -4,51 +4,7 @@ resource "aws_s3_bucket_policy" "allow_s3_access_for_ecr_viewer" {
   policy = data.aws_iam_policy_document.ecr_viewer_s3_policy.json
 }
 
-data "aws_iam_policy_document" "ecr_viewer_s3_policy" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["ecrViewerPutPostGetS3Policy"]
-    }
-
-    actions = [
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:PostObject",
-      "s3:PostObjectAcl",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-    ]
-
-    resources = [
-      aws_s3_bucket.s3.arn,
-      "${aws_s3_bucket.s3.arn}/*",
-    ]
-  }
-}
-
 resource "aws_s3_bucket_policy" "allow_s3_access_for_orchestration" {
   bucket = aws_s3_bucket.s3.id
   policy = data.aws_iam_policy_document.orchestration_s3_policy.json
-}
-
-data "aws_iam_policy_document" "orchestration_s3_policy" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["orchestrationPutAndPostS3Policy"]
-    }
-
-    actions = [
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:PostObject",
-      "s3:PostObjectAcl",
-    ]
-
-    resources = [
-      aws_s3_bucket.s3.arn,
-      "${aws_s3_bucket.s3.arn}/*",
-    ]
-  }
 }
