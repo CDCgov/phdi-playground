f4120db7ec8c67ecefc98e7c9d25af8e4ef4ee19
marycrawford <mary.crawford@skylight.digital>
2024-03-01 15:43:07 -0800

    update iam roles for s3 and eks modules

--- terraform/aws/implementation/modules/s3/iam.tf
+++ terraform/aws/implementation/modules/s3/iam.tf
@@ -1,16 +1,33 @@
-# TODO iam permissions to speak with clear
-resource "aws_s3_bucket_policy" "allow_s3_access_for_ecr_viewer" {
-  bucket = aws_s3_bucket.s3.id
-  policy = data.aws_iam_policy_document.ecr_viewer_s3_policy.json
-}
-
-resource "aws_s3_bucket_policy" "allow_s3_access_for_orchestration" {
-  bucket = aws_s3_bucket.s3.id
-  policy = data.aws_iam_policy_document.orchestration_s3_policy.json
-}
-
 # s3 bucket role
-resource "aws_iam_role" "s3_role" {
-  name               = "S3AccessRole"
-  assume_role_policy = data.aws_iam_policy_document.s3_role_policy.json
+resource "aws_iam_role" "s3_role_for_ecr_viewer" {
+  name               = "S3AccessRoleForEcrViewer"
+  assume_role_policy = var.eks_assume_role_policy
+}
+
+resource "aws_iam_policy" "s3_bucket_ecr_viewer_policy" {
+  name        = "AWSS3IAMPolicyForEcrViewer"
+  description = "Policy for ECR-Viewer and S3 in DIBBS"
+  policy      = data.aws_iam_policy_document.ecr_viewer_s3_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "s3_bucket_ecr_viewer_policy" {
+  role       = aws_iam_role.s3_role_for_ecr_viewer.name
+  policy_arn = aws_iam_policy.s3_bucket_ecr_viewer_policy.arn
+}
+
+
+resource "aws_iam_role" "s3_role_for_orchestration" {
+  name               = "S3AccessRoleForOrchestration"
+  assume_role_policy = var.eks_assume_role_policy
+}
+
+resource "aws_iam_policy" "s3_bucket_orchestration_policy" {
+  name        = "AWSS3IAMPolicyForOrchestration"
+  description = "Policy for Orchestration and S3 in DIBBS"
+  policy      = data.aws_iam_policy_document.orchestration_s3_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "s3_bucket_orchestration_policy" {
+  role       = aws_iam_role.s3_role_for_orchestration.name
+  policy_arn = aws_iam_policy.s3_bucket_orchestration_policy.arn
 }
