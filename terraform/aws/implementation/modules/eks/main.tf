c20a6603cddffc4668408fbf9d5da4d518b41e98
Mary Crawford <29112142+marycrawford@users.noreply.github.com>
2024-03-04 09:54:12 -0800

    update iam roles for s3 and eks modules (#63)
    * update iam roles for s3 and eks modules
    
    * update s3/data.tf file
    
    ---------
    
    Co-authored-by: marycrawford <mary.crawford@skylight.digital>

--- terraform/aws/implementation/modules/eks/main.tf
+++ terraform/aws/implementation/modules/eks/main.tf
@@ -134,25 +134,7 @@ locals {
 
 resource "aws_iam_role" "eks_service_account" {
   name = "AmazonEKSLoadBalancerControllerRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${local.account_id}:oidc-provider/${local.oidc_provider}"
-        }
-        Condition = {
-          StringEquals = {
-            "${local.oidc_provider}:aud" = "sts.amazonaws.com"
-            "${local.oidc_provider}:sub" = "system:serviceaccount:${local.namespace}:${local.service_account_name}"
-          }
-        }
-      },
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "load_balancer_controller" {
--- terraform/aws/implementation/modules/s3/data.tf
+++ terraform/aws/implementation/modules/s3/data.tf
@@ -1,16 +1,7 @@
 data "aws_iam_policy_document" "ecr_viewer_s3_policy" {
   statement {
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam:339712971032:role/s3_role"]
-    }
-
+    sid       = ""
+    effect    = "Allow"
     actions = [
       "s3:PutObject",
       "s3:PutObjectAcl",
@@ -29,17 +20,8 @@ data "aws_iam_policy_document" "ecr_viewer_s3_policy" {
 
 data "aws_iam_policy_document" "orchestration_s3_policy" {
   statement {
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam:339712971032:role/s3_role"]
-    }
-
+    sid       = ""
+    effect    = "Allow"
     actions = [
       "s3:PutObject",
       "s3:PutObjectAcl",
@@ -52,15 +34,3 @@ data "aws_iam_policy_document" "orchestration_s3_policy" {
       "${aws_s3_bucket.s3.arn}/*",
     ]
   }
-}
-
-data "aws_iam_policy_document" "s3_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-  }
-}
