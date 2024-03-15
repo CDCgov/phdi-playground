0d83fac6327e3456e33d2d58b28a9ae6a8d4291b
Nick Clyde <nick@clyde.tech>
2024-03-15 12:41:14 -0700

    Add external data source to grab latest Helm chart versions

--- terraform/aws/implementation/modules/eks/main.tf
+++ terraform/aws/implementation/modules/eks/main.tf
@@ -268,14 +268,28 @@ resource "terraform_data" "wait_for_load_balancer_controller" {
   }
 }
 
-# Building blocks
+# Use Helm CLI to get latest chart versions
 
+resource "terraform_data" "helm_setup" {
+  provisioner "local-exec" {
+    command = "helm repo add phdi-charts https://cdcgov.github.io/phdi-charts/ && helm repo update"
+  }
+}
+
+data "external" "chart_versions" {
+  depends_on = [terraform_data.helm_setup]
+
+  program = ["helm", "search", "repo", "phdi-charts", "-o", "json", "|", "jq", "'reduce .[] as $item ({}; .[$item.name | sub(\"^phdi-charts/\";\"\")] |= if . == null then $item.version else if . < $item.version then $item.version else . end end)'"]
+}
+
+# Building blocks
 resource "helm_release" "building_blocks" {
   depends_on      = [terraform_data.wait_for_load_balancer_controller]
   for_each        = var.services_to_chart
   repository      = "https://cdcgov.github.io/phdi-charts/"
   name            = "phdi-playground-${terraform.workspace}-${each.key}"
   chart           = each.value
+  version         = data.external.chart_versions.result[each.value]
   force_update    = true
   recreate_pods   = true
   cleanup_on_fail = true
