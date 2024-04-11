0d4618af6f88eab1a559083710bd49987c162339
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-04-11 16:39:46 -0400

    undo try at local

--- terraform/aws/implementation/modules/eks/main.tf
+++ terraform/aws/implementation/modules/eks/main.tf
@@ -432,16 +432,12 @@ module "eks_blueprints_addons" {
   tags = local.tags
 }
 
-locals {
-  otel_collector_values = file("${path.module}/otel_collector_values.yaml")
-}
-
 resource "helm_release" "otel_collector" {
   name       = "otel-collector"
   repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart      = "opentelemetry-collector"
   version    = "0.6.0"
-  values     = [local.otel_collector_values]
+  values     = [file("${path.module}/otel_collector_values.yaml")]
   # set {
   #   name  = "service.type"
   #   value = "ClusterIP"
