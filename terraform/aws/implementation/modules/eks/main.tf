0f0e4c55fdef21f9e504fda08898f4ea6f57fc42
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-04-12 15:34:12 -0400

    kubesystem

--- terraform/aws/implementation/modules/eks/main.tf
+++ terraform/aws/implementation/modules/eks/main.tf
@@ -436,7 +436,8 @@ resource "helm_release" "otel_collector" {
   name       = "otel-collector"
   repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart      = "opentelemetry-collector"
-  version    = "0.5.0"
+  version    = "0.6.0"
+  namespace  = "kube-system"
   values     = [(file("${path.module}/otel-collector-config.yaml"))]
   set {
     name  = "opentelemetry-collector.serviceEnabled"
