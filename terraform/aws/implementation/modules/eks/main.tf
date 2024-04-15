b882963aefd9da58dd5cff089059d4a3001c15d2
Rob Mitchell <robert.a.mitchell@skylight.digital>
2024-04-15 11:06:14 -0400

    falling back to 0.6.0; can't get latest to work...

--- terraform/aws/implementation/modules/eks/main.tf
+++ terraform/aws/implementation/modules/eks/main.tf
@@ -436,7 +436,7 @@ resource "helm_release" "otel_collector" {
   name       = "otel-collector"
   repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart      = "opentelemetry-collector"
-  version    = "0.86.2"
+  version    = "0.6.0"
   values     = [(file("${path.module}/otel-collector-config.yaml"))]
   set {
     name  = "opentelemetry-collector.serviceEnabled"
