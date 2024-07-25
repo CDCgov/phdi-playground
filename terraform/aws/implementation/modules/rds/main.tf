0a6203ff5a14654ccf749e3c0d045de020485b5c
marycrawford <mary.crawford@skylight.digital>
2024-07-24 23:21:31 -0700

    update db identifier to query-templates

--- terraform/aws/implementation/modules/rds/main.tf
+++ terraform/aws/implementation/modules/rds/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "query-templates" {
-  identifier        = "tefca-viewer"
+  identifier        = "query-templates"
   instance_class    = "db.t3.micro"
   allocated_storage = 5
   engine            = "postgres"
@@ -7,7 +7,7 @@ resource "aws_db_instance" "query-templates" {
   username          = var.db_username
   password          = var.db_password
   #db_subnet_group_name   = aws_db_subnet_group.education.name
-  vpc_security_group_ids = var.private_subnet_ids
+  #vpc_security_group_ids = var.private_subnet_ids
   #parameter_group_name   = aws_db_parameter_group.education.name
   publicly_accessible = false
   #skip_final_snapshot    = true
