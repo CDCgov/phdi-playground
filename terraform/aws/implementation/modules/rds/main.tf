e4176b3e7e9c592ab9d8eb454ce15f3dd624dd62
marycrawford <mary.crawford@skylight.digital>
2024-07-24 23:11:11 -0700

    initial commit for adding tefca-viewer postgres db

---
+++ terraform/aws/implementation/modules/rds/main.tf
@@ -0,0 +1,14 @@
+resource "aws_db_instance" "query-templates" {
+  identifier        = "tefca-viewer"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 5
+  engine            = "postgres"
+  engine_version    = "16.3"
+  username          = var.db_username
+  password          = var.db_password
+  #db_subnet_group_name   = aws_db_subnet_group.education.name
+  vpc_security_group_ids = var.private_subnet_ids
+  #parameter_group_name   = aws_db_parameter_group.education.name
+  publicly_accessible = false
+  #skip_final_snapshot    = true
+}
