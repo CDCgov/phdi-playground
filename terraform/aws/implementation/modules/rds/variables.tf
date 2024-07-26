ce8698fb469bf4abe0f87d17e86749f4690740f7
marycrawford <mary.crawford@skylight.digital>
2024-07-25 23:29:12 -0700

    update variables

--- terraform/aws/implementation/modules/rds/variables.tf
+++ terraform/aws/implementation/modules/rds/variables.tf
@@ -1,3 +1,10 @@
+# Note: only lowercase alphanumeric characters and hyphens allowed in "identifier"
+variable "db_identifier" {
+  type        = string
+  description = "Name of RDS Instance"
+  default     = "query-templates"
+}
+
 variable "db_username" {
   type        = string
   description = "Username of RDS Instance"
@@ -10,21 +17,12 @@ variable "db_password" {
   # TODO: Turn on sensitive once the database is fully up and save real pwd 
   # as a variable to ingest from the pipeline (i.e. Github Secrets)
   sensitive = true
-
 }
 
 
 variable "private_subnet_ids" {
   type        = list(string)
   description = "List of private subnet IDs"
-  default     = ["176.24.1.0/24", "176.24.3.0/24"]
-}
-
-# Note: only lowercase alphanumeric characters and hyphens allowed in "identifier"
-variable "db_identifier" {
-  type        = string
-  description = "Identifier Name of RDS Instance"
-  default     = "query-templates"
 }
 
 variable "region" {
@@ -32,8 +30,7 @@ variable "region" {
   default = "us-east-1"
 }
 
-# variable "vpc_id" {
-#   type        = string
-#   description = "ID of the VPC"
-#   default     = "176.24.0.0/16"
-# }
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC"
+}
