a2fc67b88f4ed9eb70df5796b87981fbec6079d8
marycrawford <mary.crawford@skylight.digital>
2024-07-29 09:54:50 -0700

    update rds variables and add password as a protected secret

--- terraform/aws/implementation/modules/rds/main.tf
+++ terraform/aws/implementation/modules/rds/main.tf
@@ -1,9 +1,10 @@
+# Define the RDS instance for Postgres
 resource "aws_db_instance" "query-templates" {
   identifier        = var.db_identifier
   instance_class    = "db.t3.micro"
   allocated_storage = 5
-  engine            = "postgres"
-  engine_version    = "16.3"
+  engine            = var.engine_type
+  engine_version    = var.engine_version
   username          = var.db_username
   password          = var.db_password
   #db_subnet_group_name   = aws_db_subnet_group.this.name
@@ -12,11 +13,14 @@ resource "aws_db_instance" "query-templates" {
   publicly_accessible       = false
   skip_final_snapshot       = true
   final_snapshot_identifier = true
+  # backup_window             = var.backup_window
+  # maintenance_window        = var.maintenance_window
 }
 
+# Create a parameter group to configure Postgres RDS parameters
 resource "aws_db_parameter_group" "this" {
   name   = "${var.db_identifier}-pg"
-  family = "postgres16"
+  family = var.family
 
   parameter {
     name  = "log_connections"
@@ -26,4 +30,36 @@ resource "aws_db_parameter_group" "this" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+# Security group for RDS
+resource "aws_security_group" "ds_sg" {
+  vpc_id = module.vpc.vpc_id
+
+  # Allow inbound traffic on port 5432 for PostgreSQL from within the VPC
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.db_identifier}-security-group"
+  }
+}
+
+# Create a DB subnet group
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.db_identifier}-subnet-group"
+  subnet_ids = module.vpc.private_subnet_ids
+
 }
--- terraform/aws/implementation/modules/rds/variables.tf
+++ terraform/aws/implementation/modules/rds/variables.tf
@@ -1,3 +1,10 @@
+#Must be in the format of "hh24:mi-hh24:mi"
+variable "backup_window" {
+  type        = string
+  description = "Backup Window for RDS Instance"
+  default     = "03:00-06:00"
+}
+
 # Note: only lowercase alphanumeric characters and hyphens allowed in "identifier"
 variable "db_identifier" {
   type        = string
@@ -15,6 +22,31 @@ variable "db_password" {
   type        = string
   description = "Password of RDS Instance"
   sensitive   = true
+}
+
+variable "engine_type" {
+  type        = string
+  description = "Engine of RDS Instance"
+  default     = "postgres"
+}
+
+variable "engine_version" {
+  type        = string
+  description = "Engine Version of RDS Instance"
+  default     = "16.3"
+}
+
+variable "family" {
+  type        = string
+  description = "RDS Family"
+  default     = "postgres16"
+}
+
+# Must be in the format of "ddd:hh24:mi-ddd:hh24:mi"
+variable "maintenance_window" {
+  type        = string
+  description = "Maintenance Window for RDS Instance"
+  default     = "Mon:00:00mi-Mon:3:00:mi"
 }
 
 variable "private_subnet_ids" {
